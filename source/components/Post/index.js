fb686c3526b62c7b94bb709699820e322038ec0f
Vladyslav Kulyk <vladyslav.kulyk@netent.com>
2019-01-23 10:45:35 +0200

    Lesson 4. React-Context

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -2,20 +2,25 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+// Components
+import { Consumer } from 'components/HOC/withProfile';
+
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Post extends Component {
     render() {
-        const {currentUserFirstName, currentUserLastName, avatar} = this.props;
-
         return (
-            <section className = { Styles.post }>
-                <img src = { avatar } />
-                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
-                <time>{moment().format('MMMM D h:mm:ss a')}</time>
-                <p>Howdy!</p>
-            </section>
+            <Consumer>
+                {({currentUserFirstName, currentUserLastName, avatar}) => (
+                    <section className = { Styles.post }>
+                        <img src = { avatar } />
+                        <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
+                        <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                        <p>Howdy!</p>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,21 +1,27 @@
 // Core
 import React, { Component } from 'react';
 
+// Components
+import { Consumer } from 'components/HOC/withProfile';
+
+// Instruments
 import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
     render() {
-        const {currentUserFirstName, currentUserLastName, avatar} = this.props;
-
         return (
-            <section className = { Styles.statusBar }>
-                <button>
-                    <img src = { avatar } />
-                    <span>{currentUserFirstName}</span>
-                    &nbsp;
-                    <span>{currentUserLastName}</span>
-                </button>
-            </section>
+            <Consumer>
+                {({currentUserFirstName, currentUserLastName, avatar}) => (
+                    <section className = { Styles.statusBar }>
+                        <button>
+                            <img src = { avatar } />
+                            <span>{currentUserFirstName}</span>
+                            &nbsp;
+                            <span>{currentUserLastName}</span>
+                        </button>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
